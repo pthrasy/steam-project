2c83035a055f9dcd348da313d387e528505db372
pthrasy <127073204+pthrasy@users.noreply.github.com>
2025-12-06 19:10:15 -0500

    Refactor Streamlit dashboard to include user input

--- app.py
+++ app.py
@@ -8,10 +8,19 @@ import pandas as pd
 import streamlit as st
 import plotly.express as px
 import os
+from fetch_steam_data import fetch_all_data
 
 st.set_page_config(page_title="Steam Game Analytics", layout="wide")
 st.title(" Steam Data Dashboard")
+user_input = st.text_input("Enter user name or Steam ID")
 
+if st.button("Fetch Stats"):
+    try:
+        data = fetch_all_data(user_input)
+        st.json(data)
+    except Exception as e: 
+        st.error(str(e())
+                 
 #loads cached data
 if not os.path.exists("data.json"):
     st.error("No data json found. Run fetch_steam_data.py first")
@@ -27,4 +36,5 @@ if not games:
 else: 
     #convert list to DataFrame
     df = pd.DataFrame(games)
+
     df["playtime_hours"] 
