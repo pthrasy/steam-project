4c667bf6185b6563af7003a65c192fd8c42616df
pthrasy <127073204+pthrasy@users.noreply.github.com>
2026-01-07 10:57:00 -0500

    Update app.py

--- app.py
+++ app.py
@@ -30,14 +30,43 @@ st.write("Method 1 (getenv):", API_KEY is not None)
 if not os.path.exists("data.json"):
     st.error("No data json found. Run fetch_steam_data.py first")
     st.stop()
+def extract_steam_data(data):
+    """Extract all steam data from the JSON structure""" 
+    result = {
+        'steam_id': data.get('steam_id', 'unknown'); 
+        'profile': None,
+        'games': [],
+        'recent_games': [],
+        'friends': []
+        'global_achievements': {},
+        'player_achievements': {}
+    }
+if 'profile' in data and 'response' in data['profile']:
+    players = data['profile']['response'].get('players', [])
+    if players:
+        result['profile'] = players[0]
+if 'owned_games' in data and 'response' in data['owned_games']:
+    result['games'] = data['owned_games']['response'].get('games',[])
+
+if 'recently_played' in data and 'repsonse' in data['recently_played']:
+    result['result_games'] = data['recently_played']['response'].get('games',[])
+
+if 'friends' in data and 'friendslist' in data['friends']:
+    result['friends'] = data['friends']['friendlist'].get('friends',[])
+
+result['global_achievements'] = data.get('global_achievements', {})
+result['player_achievements'] = data.get('player_achievements', {})
+return result 
 
 with open("data.json", "r") as f:
     data = json.load(f)
+steam_data = extract_steam_data(data)
 
-if 'response' in data:
-    games = data['response'].get("games", [])
-elif 'games' in data:
-    games = data.get("games", [])
+#where you access by convienence
+profile = steam_data['profile']
+games = steam_data['games']
+friends = steam_data['friends']
+
 
 if not games:
     st.warning("No games found, make sure that your profile is public and that you play something. blah blajh blah")
@@ -46,6 +75,7 @@ else:
     df = pd.DataFrame(games)
 
     df["playtime_hours"] 
+
 
 
 
