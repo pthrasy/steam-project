6188fa701a19cc077c8d9a4a4bd2688460f8e1ad
pthrasy <127073204+pthrasy@users.noreply.github.com>
2026-04-28 15:13:10 -0400

    Update welcome_page.py

--- welcome_page.py
+++ welcome_page.py
@@ -8,9 +8,7 @@ st.header("This is my portfolio page")
 st.write("Hello, my name is Paul Thrasybule and this is my portfolio page! Whether you are an investor, graduate program supervisor, or simply got lost, you ended up here. I would like to use this platform to show off more of my personality and achievements.")
 st.header("Background")
 st.write("I grew up in Maryland my whole life, to a dual immigrant household; half Indian and half Hatian. It brought me into a world of culture where I was able to see a voracious spread of food, music, and especially art. To me, art is a wonderful expression of intellegence and perception tied together.")
-st.write("Learning about data science, it had that same sense of awe, where I could see all types of data: demographics, genres, countries all be represented in a confined table. 
-st.write("
-    
+st.write("Learning about data science, it had that same sense of awe, where I could see all types of data: demographics, genres, countries all be represented in a confined table.")
 st.header("My Steam API Project")
 st.link_button("Visit my Steam API Project", "https://steam-project-mbsqh6gaty8uknmneuwyfm.streamlit.app/")
 st.write("One of the first things my dad ever got me was a Dell Laptop. Given to me in 2010, it was used tobe able to practice typing, and from there I had learned so much since.")
