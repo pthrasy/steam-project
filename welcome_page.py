e6e7e5bc1b12b58ff1a211289530a7ebaf286152
pthrasy <127073204+pthrasy@users.noreply.github.com>
2026-04-28 13:53:00 -0400

    Create welcome_page.py

---
+++ welcome_page.py
@@ -0,0 +1,8 @@
+import streamlit as st
+
+st.set_page_config(
+  page_title="Welcome"
+)
+
+st.write("This is my portfolio page")
+st.link_button("Visit my Steam API Project", "https://steam-project-mbsqh6gaty8uknmneuwyfm.streamlit.app/")
