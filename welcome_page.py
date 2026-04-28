a7bad7b7a5fca98b1f361ac67151183cbf124319
pthrasy <127073204+pthrasy@users.noreply.github.com>
2026-04-28 14:55:00 -0400

    Update welcome_page.py

--- welcome_page.py
+++ welcome_page.py
@@ -4,5 +4,17 @@ st.set_page_config(
   page_title="Welcome"
 )
 
-st.write("This is my portfolio page")
+st.header("This is my portfolio page")
+st.write("Hello, my name is Paul Thrasybule and this is my portfolio page! Whether you are an investor, graduate program supervisor, or simply got lost,
+         you ended up here. I would like to use this platform to show off more of my personality and achievements.")
+st.header("Background")
+st.write("I grew up in Maryland my whole life, to a dual immigrant household; half Indian and half Hatian. It brought me into a world of culture where I was 
+able to see a voracious spread of food, music, and especially art. To me, art is a wonderful expression of intellegence and perception tied together to create
+display.
+st.header("My Steam API Project")
 st.link_button("Visit my Steam API Project", "https://steam-project-mbsqh6gaty8uknmneuwyfm.streamlit.app/")
+st.write("One of the first things my dad ever got me was a Dell Laptop. Given to me in 2010, it was used to
+be able to practice typing, and from there I had learned so much since. Since then much time was spent gaming, and once I had a steam account in 2014, I had started
+to accumulate time and achievements online. Wondering where a good data science project might be, I had found one literally right in front of me.
+This project taught me alot of the creation and development of APIs and their utility. In particular, how limiting an API can be based on its maximum amount of
+data calls and types of data that are accessible."
